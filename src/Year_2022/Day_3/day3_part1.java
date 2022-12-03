01c6b09af27a7b6b4495adf15acb85c33e8e1cf2
pillwein <9132tl.rennweg.at>
2022-12-03 09:46:33 +0100

    day 3 done

--- src/Year_2022/Day_3/day3_part1.java
+++ src/Year_2022/Day_3/day3_part1.java
@@ -10,18 +10,32 @@ import java.awt.datatransfer.Clipboard;
 public class day3_part1 {
 
     public static void main(String[] args) throws IOException {
-        String received = methodName("src/Year_2022/Day_3/source.txt")+"";
+        String received = RucksackScore("src/Year_2022/Day_3/source.txt")+"";
         copyToClipboard(received);
         System.out.println("----------------------------------------------------");
         System.out.println("FINAL RESULT: "+received);
     }
 
-    public static long methodName(String path) throws IOException {
+    public static long RucksackScore(String path) throws IOException {
         String[] file = Files.readAllLines(Path.of(path)).toArray(new String[0]);
+        long score = 0;
         for (int i = 0; i < file.length; i++) {
-            System.out.println(file[i]);
+            String compartment1 = file[i].substring(0,file[i].length()/2);
+            String compartment2 = file[i].substring(file[i].length()/2);
+            for (int ii = 0; ii < compartment1.length(); ii++) {
+                if (compartment2.contains(compartment1.charAt(ii)+"")){
+                    score += getPriority(compartment1.charAt(ii));
+                    break;
+                }
+            }
+
         }
-        return 0;
+        return score;
+    }
+
+    public static long getPriority(char input){
+        if (Character.isLowerCase(input)) return (input+0)-96;
+        else return (input+0)-38;
     }
 
     public static void copyToClipboard(String received) {
--- src/Year_2022/Day_3/day3_part2.java
+++ src/Year_2022/Day_3/day3_part2.java
@@ -10,18 +10,33 @@ import java.awt.datatransfer.Clipboard;
 public class day3_part2 {
 
     public static void main(String[] args) throws IOException {
-        String received = methodName("src/Year_2022/Day_3/source.txt")+"";
+        String received = RucksackScore("src/Year_2022/Day_3/source.txt")+"";
         copyToClipboard(received);
         System.out.println("----------------------------------------------------");
         System.out.println("FINAL RESULT: "+received);
     }
 
-    public static long methodName(String path) throws IOException {
+    public static long RucksackScore(String path) throws IOException {
         String[] file = Files.readAllLines(Path.of(path)).toArray(new String[0]);
-        for (int i = 0; i < file.length; i++) {
-            System.out.println(file[i]);
+        long score = 0;
+        for (int i = 0; i < file.length; i+=3) {
+            String rucksack1 = file[i];
+            String rucksack2 = file[i+1];
+            String rucksack3 = file[i+2];
+            for (int ii = 0; ii < rucksack1.length(); ii++) {
+                if (rucksack2.contains(rucksack1.charAt(ii)+"") && rucksack3.contains(rucksack1.charAt(ii)+"")){
+                    score += getPriority(rucksack1.charAt(ii));
+                    break;
+                }
+            }
+
         }
-        return 0;
+        return score;
+    }
+
+    public static long getPriority(char input){
+        if (Character.isLowerCase(input)) return (input+0)-96;
+        else return (input+0)-38;
     }
 
     public static void copyToClipboard(String received) {
