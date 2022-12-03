d199254a9e3656144a161e5433dc1c6ed33d3371
pillwein <9132tl.rennweg.at>
2022-12-03 09:50:21 +0100

    day 4 created

---
+++ src/Year_2022/Day_4/day4_part1.java
@@ -0,0 +1,32 @@
+package Year_2022.Day_4;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.awt.datatransfer.StringSelection;
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+
+public class day4_part1 {
+
+    public static void main(String[] args) throws IOException {
+        String received = methodName("src/Year_2022/Day_4/source.txt")+"";
+        copyToClipboard(received);
+        System.out.println("----------------------------------------------------");
+        System.out.println("FINAL RESULT: "+received);
+    }
+
+    public static long methodName(String path) throws IOException {
+        String[] file = Files.readAllLines(Path.of(path)).toArray(new String[0]);
+        for (int i = 0; i < file.length; i++) {
+            System.out.println(file[i]);
+        }
+        return 0;
+    }
+
+    public static void copyToClipboard(String received) {
+        StringSelection stringSelection = new StringSelection(received);
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        clipboard.setContents(stringSelection, null);
+    }
+}
