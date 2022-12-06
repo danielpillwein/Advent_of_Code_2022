b90d27d2c97eaad1e310b0837c45af15a24e3ffe
danielpillwein <9132@htl.rennweg.at>
2022-12-06 17:33:48 +0100

    day 7 created

--- src/CreateNewDay.java
+++ src/CreateNewDay.java
@@ -5,7 +5,7 @@ import java.time.LocalDateTime;
 
 public class CreateNewDay {
     public static void main(String[] args) throws IOException {
-        createNewDay(06);
+        createNewDay(07);
     }
 
     public static void createNewDay(int ... dayOfMonth) throws IOException {
