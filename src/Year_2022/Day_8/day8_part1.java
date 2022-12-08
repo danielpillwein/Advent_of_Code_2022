34d0e2ae922a2b09c33f940089981735e1e9e568
danielpillwein <9132@htl.rennweg.at>
2022-12-08 16:06:22 +0100

    day 8 done

--- src/Year_2022/Day_8/day8_part1.java
+++ src/Year_2022/Day_8/day8_part1.java
@@ -10,18 +10,59 @@ import java.awt.datatransfer.Clipboard;
 public class day8_part1 {
 
     public static void main(String[] args) throws IOException {
-        String received = methodName("src/Year_2022/Day_8/source.txt")+"";
+        String received = visibleTrees("src/Year_2022/Day_8/source.txt")+"";
         copyToClipboard(received);
         System.out.println("----------------------------------------------------");
         System.out.println("FINAL RESULT: "+received);
     }
 
-    public static long methodName(String path) throws IOException {
+    public static long visibleTrees(String path) throws IOException {
         String[] file = Files.readAllLines(Path.of(path)).toArray(new String[0]);
+        int[][] input = readInput(file);
+        long counter = 0;
+
         for (int i = 0; i < file.length; i++) {
-            System.out.println(file[i]);
+            for (int ii = 0; ii < file[i].length(); ii++) {
+                if (checkLeft(input,i,ii) || checkRight(input,i,ii) || checkTop(input,i,ii) || checkBottom(input,i,ii)) counter++;
+            }
         }
-        return 0;
+        return counter;
+    }
+
+    public static boolean checkLeft(int[][] input, int row, int column){
+        for (int i = 0; i < column; i++) {
+            if (input[row][i]>=input[row][column]) return false;
+        }
+        return true;
+    }
+    public static boolean checkRight(int[][] input, int row, int column){
+        for (int i = input[row].length-1; i > column; i--) {
+            if (input[row][i]>=input[row][column]) return false;
+        }
+        return true;
+    }
+
+    public static boolean checkTop(int[][] input, int row, int column){
+        for (int i = 0; i < row; i++) {
+            if (input[i][column]>=input[row][column]) return false;
+        }
+        return true;
+    }
+
+    public static boolean checkBottom(int[][] input, int row, int column){
+        for (int i = input.length-1; i > row; i--) {
+            if (input[i][column]>=input[row][column]) return false;
+        }
+        return true;
+    }
+    public static int[][] readInput(String[] file){
+        int[][] input = new int[file.length][file[0].length()];
+        for (int i = 0; i < file.length; i++) {
+            for (int ii = 0; ii < file[i].length(); ii++) {
+                input[i][ii] = Integer.parseInt(file[i].charAt(ii)+"");
+            }
+        }
+        return input;
     }
 
     public static void copyToClipboard(String received) {
--- src/Year_2022/Day_8/day8_part2.java
+++ src/Year_2022/Day_8/day8_part2.java
@@ -10,19 +10,73 @@ import java.awt.datatransfer.Clipboard;
 public class day8_part2 {
 
     public static void main(String[] args) throws IOException {
-        String received = methodName("src/Year_2022/Day_8/source.txt")+"";
+        String received = highestScenicScore("src/Year_2022/Day_8/source.txt")+"";
         copyToClipboard(received);
         System.out.println("----------------------------------------------------");
         System.out.println("FINAL RESULT: "+received);
     }
 
-    public static long methodName(String path) throws IOException {
+    public static long highestScenicScore(String path) throws IOException {
         String[] file = Files.readAllLines(Path.of(path)).toArray(new String[0]);
+        int[][] input = readInput(file);
+        long counter = 0;
+        
         for (int i = 0; i < file.length; i++) {
-            System.out.println(file[i]);
+            for (int ii = 0; ii < file[i].length(); ii++) {
+                long score = viewLeft(input,i,ii)*viewRight(input,i,ii)*viewTop(input,i,ii)*viewBottom(input,i,ii);
+                counter = Math.max(score,counter);
+            }
         }
-        return 0;
+        return counter;
     }
+
+    public static long viewLeft(int[][] input, int row, int column){
+        long viewDistance = 0;
+        if (column-1<0) return 0;
+        for (int i = column-1; i >= 0; i--) {
+            if (input[row][i]<input[row][column]) viewDistance++;
+            else if (input[row][i]>=input[row][column]) return ++viewDistance;
+        }
+        return viewDistance;
+    }
+    public static long viewRight(int[][] input, int row, int column){
+        long viewDistance = 0;
+        if (column+1>input[row].length) return 0;
+        for (int i = column+1; i < input[row].length; i++) {
+            if (input[row][i]<input[row][column]) viewDistance++;
+            else if (input[row][i]>=input[row][column]) return ++viewDistance;
+        }
+        return viewDistance;
+    }
+    public static long viewTop(int[][] input, int row, int column){
+        long viewDistance = 0;
+        if (row-1<0) return 0;
+        for (int i = row-1; i >= 0; i--) {
+            if (input[i][column]<input[row][column]) viewDistance++;
+            else if (input[i][column]>=input[row][column]) return ++viewDistance;
+        }
+        return viewDistance;
+    }
+
+    public static long viewBottom(int[][] input, int row, int column){
+        long viewDistance = 0;
+        if (row+1>input.length) return 0;
+        for (int i = row+1; i < input.length; i++) {
+            if (input[i][column]<input[row][column]) viewDistance++;
+            else if (input[i][column]>=input[row][column]) return ++viewDistance;
+        }
+        return viewDistance;
+    }
+    public static int[][] readInput(String[] file){
+        int[][] input = new int[file.length][file[0].length()];
+        for (int i = 0; i < file.length; i++) {
+            for (int ii = 0; ii < file[i].length(); ii++) {
+                input[i][ii] = Integer.parseInt(file[i].charAt(ii)+"");
+            }
+        }
+        return input;
+    }
+
 
     public static void copyToClipboard(String received) {
         StringSelection stringSelection = new StringSelection(received);
