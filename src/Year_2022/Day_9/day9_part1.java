b8c451601e05bcf2967b272fd891fd95f0124821
danielpillwein <9132@htl.rennweg.at>
2022-12-09 13:12:16 +0100

    day 9 done

--- src/Year_2022/Day_9/day9_part1.java
+++ src/Year_2022/Day_9/day9_part1.java
@@ -6,22 +6,64 @@ import java.nio.file.Path;
 import java.awt.datatransfer.StringSelection;
 import java.awt.Toolkit;
 import java.awt.datatransfer.Clipboard;
+import java.util.LinkedList;
+import java.util.List;
 
 public class day9_part1 {
 
     public static void main(String[] args) throws IOException {
-        String received = methodName("src/Year_2022/Day_9/source.txt")+"";
+        String received = methodName("src/Year_2022/Day_9/source.txt") + "";
         copyToClipboard(received);
         System.out.println("----------------------------------------------------");
-        System.out.println("FINAL RESULT: "+received);
+        System.out.println("FINAL RESULT: " + received);
     }
 
     public static long methodName(String path) throws IOException {
         String[] file = Files.readAllLines(Path.of(path)).toArray(new String[0]);
-        for (int i = 0; i < file.length; i++) {
-            System.out.println(file[i]);
+        int[] head = new int[]{0, 0};
+        int[] tail = new int[]{0, 0};
+        List<String> positionsVisited = new LinkedList<>();
+
+        for (String s : file) {
+            char direction = s.charAt(0);
+            long steps = Long.parseLong(s.split(" ")[1]);
+            for (long step = 0; step < steps; step++) {
+                switch (direction) {
+                    case 'U' -> head[1]++;
+                    case 'R' -> head[0]++;
+                    case 'D' -> head[1]--;
+                    case 'L' -> head[0]--;
+                }
+                updateTail(head, tail);
+                if (!positionsVisited.contains(tail[0] + ":" + tail[1])) positionsVisited.add(tail[0] + ":" + tail[1]);
+            }
         }
-        return 0;
+        return positionsVisited.size();
+    }
+
+    public static int[] updateTail(int[] head, int[] tail) {
+        long xDiff = Math.abs(head[0] - tail[0]);
+        long yDiff = Math.abs(head[1] - tail[1]);
+        if (Math.sqrt(Math.pow(xDiff, 2) + Math.pow(yDiff, 2)) > Math.sqrt(2)) {
+            if (xDiff == 0) {
+                tail[1] += head[1] > tail[1] ? 1 : -1;
+            } else if (yDiff == 0) {
+                tail[0] += head[0] > tail[0] ? 1 : -1;
+            } else if (head[0] < tail[0] && head[1] < tail[1]) {
+                tail[0]--;
+                tail[1]--;
+            } else if (head[0] > tail[0] && head[1] < tail[1]) {
+                tail[0]++;
+                tail[1]--;
+            } else if (head[0] > tail[0] && head[1] > tail[1]) {
+                tail[0]++;
+                tail[1]++;
+            } else if (head[0] < tail[0] && head[1] > tail[1]) {
+                tail[0]--;
+                tail[1]++;
+            }
+        }
+        return tail;
     }
 
     public static void copyToClipboard(String received) {
--- src/Year_2022/Day_9/day9_part2.java
+++ src/Year_2022/Day_9/day9_part2.java
@@ -6,6 +6,9 @@ import java.nio.file.Path;
 import java.awt.datatransfer.StringSelection;
 import java.awt.Toolkit;
 import java.awt.datatransfer.Clipboard;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 public class day9_part2 {
 
@@ -18,10 +21,53 @@ public class day9_part2 {
 
     public static long methodName(String path) throws IOException {
         String[] file = Files.readAllLines(Path.of(path)).toArray(new String[0]);
-        for (int i = 0; i < file.length; i++) {
-            System.out.println(file[i]);
+        int[][] snake = initSnake();
+        List<String> positionsVisited = new LinkedList<>();
+
+        for (String s : file) {
+            char direction = s.charAt(0);
+            long steps = Long.parseLong(s.split(" ")[1]);
+            for (long step = 0; step < steps; step++) {
+                switch (direction) {
+                    case 'U' -> snake[0][1]++;
+                    case 'R' -> snake[0][0]++;
+                    case 'D' -> snake[0][1]--;
+                    case 'L' -> snake[0][0]--;
+                }
+                for (int ii = 0; ii < snake.length - 1; ii++) {
+                    updateTail(snake[ii], snake[ii + 1]);
+                }
+                if (!positionsVisited.contains(snake[9][0] + ":" + snake[9][1]))
+                    positionsVisited.add(snake[9][0] + ":" + snake[9][1]);
+            }
         }
-        return 0;
+        return positionsVisited.size();
+    }
+
+    public static int[][] initSnake(){
+        int[][] erg = new int[10][2];
+        for (int i = 0; i < erg.length; i++) {
+            Arrays.fill(erg[i], 0);
+        }
+        return erg;
+    }
+
+    public static int[] updateTail(int[] head, int[] tail) {
+        long xDiff = Math.abs(head[0] - tail[0]);
+        long yDiff = Math.abs(head[1] - tail[1]);
+        if (Math.sqrt(Math.pow(xDiff, 2) + Math.pow(yDiff, 2)) > Math.sqrt(2)) {
+            if (xDiff == 0) {
+                tail[1] += head[1] > tail[1] ? 1 : -1;
+            }
+            else if (yDiff == 0) {
+                tail[0] += head[0] > tail[0] ? 1 : -1;
+            }
+            else if (head[0]<tail[0] && head[1]<tail[1]){tail[0]--;tail[1]--;}
+            else if (head[0]>tail[0] && head[1]<tail[1]){tail[0]++;tail[1]--;}
+            else if (head[0]>tail[0] && head[1]>tail[1]){tail[0]++;tail[1]++;}
+            else if (head[0]<tail[0] && head[1]>tail[1]){tail[0]--;tail[1]++;}
+        }
+        return tail;
     }
 
     public static void copyToClipboard(String received) {
