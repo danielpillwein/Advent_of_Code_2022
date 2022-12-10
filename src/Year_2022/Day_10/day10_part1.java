2c3f04abc5031feb3a1c552b09aa2423f7ee41be
danielpillwein <9132@htl.rennweg.at>
2022-12-10 14:50:42 +0100

    day 10 done

--- src/Year_2022/Day_10/day10_part1.java
+++ src/Year_2022/Day_10/day10_part1.java
@@ -6,22 +6,36 @@ import java.nio.file.Path;
 import java.awt.datatransfer.StringSelection;
 import java.awt.Toolkit;
 import java.awt.datatransfer.Clipboard;
+import java.util.LinkedList;
+import java.util.List;
 
 public class day10_part1 {
 
     public static void main(String[] args) throws IOException {
-        String received = methodName("src/Year_2022/Day_10/source.txt")+"";
+        String received = registerValue("src/Year_2022/Day_10/source.txt")+"";
         copyToClipboard(received);
         System.out.println("----------------------------------------------------");
         System.out.println("FINAL RESULT: "+received);
     }
 
-    public static long methodName(String path) throws IOException {
+    public static long registerValue(String path) throws IOException {
         String[] file = Files.readAllLines(Path.of(path)).toArray(new String[0]);
+        long cycle = 0;
+        long x = 1;
+        long nextLogCycle = 20;
+        List<Long> signalValues = new LinkedList<>();
         for (int i = 0; i < file.length; i++) {
-            System.out.println(file[i]);
+            if (!file[i].equals("noop")){
+                cycle+=2;
+                if (cycle>= nextLogCycle){
+                    signalValues.add(x*nextLogCycle);
+                    nextLogCycle+=40;
+                }
+                x += Long.parseLong(file[i].split(" ")[1]);
+
+            }else cycle++;
         }
-        return 0;
+        return signalValues.stream().mapToLong(y->y).sum();
     }
 
     public static void copyToClipboard(String received) {
--- src/Year_2022/Day_10/day10_part2.java
+++ src/Year_2022/Day_10/day10_part2.java
@@ -6,22 +6,50 @@ import java.nio.file.Path;
 import java.awt.datatransfer.StringSelection;
 import java.awt.Toolkit;
 import java.awt.datatransfer.Clipboard;
+import java.util.Arrays;
 
 public class day10_part2 {
 
     public static void main(String[] args) throws IOException {
-        String received = methodName("src/Year_2022/Day_10/source.txt")+"";
+        String received = fixDisplay("src/Year_2022/Day_10/source.txt")+"";
         copyToClipboard(received);
         System.out.println("----------------------------------------------------");
         System.out.println("FINAL RESULT: "+received);
     }
 
-    public static long methodName(String path) throws IOException {
+    public static long fixDisplay(String path) throws IOException {
         String[] file = Files.readAllLines(Path.of(path)).toArray(new String[0]);
+        long cycle = 0;
+        long x = 1;
+        long[][] screen = init();
         for (int i = 0; i < file.length; i++) {
-            System.out.println(file[i]);
+            if (!file[i].equals("noop")){
+                screen[(int) (cycle/40)][(int) (cycle%40)] = cycle%40>=x-1 && cycle%40<=x+1?1:0;
+                cycle++;
+                screen[(int) (cycle/40)][(int) (cycle%40)] = cycle%40>=x-1 && cycle%40<=x+1?1:0;
+                cycle++;
+                x += Long.parseLong(file[i].split(" ")[1]);
+            }else {
+                screen[(int) (cycle/40)][(int) (cycle%40)] = cycle%40>=x-1 && cycle%40<=x+1?1:0;
+                cycle++;
+            }
+        }
+        for (long[] row : screen) {
+            for (int i = 0; i < row.length; i++) {
+                if (row[i]==1) System.out.print("# ");
+                else System.out.print(". ");
+            }
+            System.out.println();
         }
         return 0;
+    }
+
+    public static long[][] init(){
+        long[][] erg = new long[6][40];
+        for (long[] row : erg) {
+            Arrays.fill(row,0);
+        }
+        return erg;
     }
 
     public static void copyToClipboard(String received) {
